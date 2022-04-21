2a7664e825337f9306aad2c845535cfad120f293
Parse-Vader <79918104+Parse-Vader@users.noreply.github.com>
2022-04-21 10:02:35 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import * as React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import './App.css';
+import data from "./data/lonlat.json"
 
 
 function App() {
+
+  console.log(data);
+
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer center={[37.6690, -84.6514]} zoom={13} scrollWheelZoom={false}>
     <TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
